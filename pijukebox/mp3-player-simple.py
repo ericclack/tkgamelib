7edf8dc0f8bf478179dc6c0a6428886f69f66cbe
Eric Clack <eric@bright-interactive.com>
2015-01-02 19:49:34 +0000

    Added copyright

--- pijukebox/mp3-player-simple.py
+++ pijukebox/mp3-player-simple.py
@@ -1,3 +1,6 @@
+# Copyright 2014, Eric Clack, eric@bn7.net
+# This program is distributed under the terms of the GNU General Public License
+
 import os
 import pibrella
 
--- pijukebox/mp3-player.py
+++ pijukebox/mp3-player.py
@@ -1,3 +1,5 @@
+# Copyright 2014, Eric Clack, eric@bn7.net
+# This program is distributed under the terms of the GNU General Public License
 
 import os
 import pibrella
