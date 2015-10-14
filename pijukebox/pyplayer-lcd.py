b7c271fadb1d09ea3d54270a1ce4bcf91e82b4e8
Eric Clack <ericclack@googlemail.com>
2015-10-14 15:20:01 +0000

    Added new pi player with LCD screen

---
+++ pijukebox/pyplayer-lcd.py
@@ -0,0 +1,69 @@
+import os, time
+
+import Adafruit_CharLCD as LCD
+
+lcd = LCD.Adafruit_CharLCDPlate()
+lcd.set_backlight(0.5)
+
+def mpc(command):
+    output = os.popen("mpc %s" % command)
+    s = output.read()
+    output.close()
+    return s
+
+def lcd_display(m):
+    lcd.clear()
+    # Wrap long messages
+    if len(m)>16:
+        m = m[:16] + "\n" + m[16:]
+    lcd.message(m)
+
+def display_current():
+    lcd_display(mpc("current"))
+
+def next_song():
+    mpc("next")
+    display_current()
+
+def prev_song():
+    mpc("prev")
+    display_current()
+
+def current_album():
+    return mpc("current").split()[0]
+
+def next_album():
+    start = current_album()
+    for go in range(100):
+        next_song()
+        if current_album() != start:
+            return
+        
+def are_we_shutting_down():
+    """Called after pause button pressed"""
+    time.sleep(0.5)
+    if lcd.is_pressed(LCD.SELECT):        
+        lcd_display("Keep pressing to shutdown!")
+        time.sleep(1)
+        if lcd.is_pressed(LCD.SELECT):
+            lcd_display("Shutting down...")
+            time.sleep(1)
+            #TODO: actually shutdown here!
+        else:
+            display_current()
+
+display_current()
+
+while True:
+    if lcd.is_pressed(LCD.RIGHT):
+        next_song()
+    if lcd.is_pressed(LCD.LEFT):
+        prev_song()
+    if lcd.is_pressed(LCD.DOWN):
+        next_album()
+    if lcd.is_pressed(LCD.SELECT):
+        mpc("toggle")
+        are_we_shutting_down()
+
+    # Don't hog the CPU
+    time.sleep(0.05)
