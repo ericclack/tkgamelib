d91dab75139e06b62749fe19a5b569e9d7079a24
Eric Clack <eric@bright-interactive.com>
2015-12-19 12:44:10 +0000

    Reworked to use new Sprite class

---
+++ examples/tk-scratch-stars1.py
@@ -0,0 +1,44 @@
+# Copyright 2014, Eric Clack, eric@bn7.net
+# This program is distributed under the terms of the GNU General Public License
+
+"""Test out of python for Racket stars game"""
+
+import random, time
+from geekclub.pyscratch import *
+
+create_canvas()
+
+STARS = []
+MAX_STARS = 100
+
+def new_star():
+    x, y = random.randint(0, CANVAS_WIDTH), random.randint(0, CANVAS_HEIGHT)
+    star = Sprite(canvas().create_oval(x, y, x+5, y+5))
+    return star
+
+def star_out_of_view(star):
+    x, y = star.x(), star.y()
+    return (x < 0 or x > CANVAS_WIDTH or y < 0 or y > CANVAS_HEIGHT)
+
+def fly_forwards():
+    global STARS
+    STARS = move_stars(STARS)
+
+def move_stars(stars):
+    stars2 = []
+    for star in stars:
+        if star_out_of_view(star):
+            star.delete()
+            stars2.append(new_star())
+        else:            
+            dx = (star.x() - CANVAS_WIDTH/2) / (CANVAS_WIDTH/50)
+            dy = (star.y() - CANVAS_HEIGHT/2) / (CANVAS_HEIGHT/50)
+            star.move(dx, dy)
+            stars2.append(star)
+    return stars2
+
+for i in range(MAX_STARS):
+    STARS.append(new_star())
+        
+forever(fly_forwards, 50)
+mainloop()
