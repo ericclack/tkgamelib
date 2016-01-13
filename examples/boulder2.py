ae1eb41e089b8c35c0fcd13d1a7ab51f5819b369
Eric Clack <eric@bright-interactive.com>
2016-01-13 20:47:38 +0000

    Gems to collect + end game status

--- examples/boulder2.py
+++ examples/boulder2.py
@@ -5,10 +5,10 @@
 
 DONE:
 - Push boulders left or right
-
-TODO:
 - Stop going off edge of screen
 - Gems to collect
+
+TODO:
 - Die if a boulder falls on you
 - Aliens
 """
@@ -16,23 +16,24 @@ TODO:
 import random, time, sys
 from geekclub.pyscratch import *
 
+BLOCK_SIZE=50
+SCREEN_SIZE=16
+
 create_canvas()
 
 block_images = {
     'mud': PhotoImage(file='images/earth.gif'),
     'boulder': PhotoImage(file='images/ball.gif'),
     'wall': PhotoImage(file='images/wall.gif'),
+    'gem': PhotoImage(file='images/gem.gif'),
 }
 
 fred_img = PhotoImage(file='images/smallface.gif')
 
-
-BLOCK_SIZE=50
-SCREEN_SIZE=16
-
 class BlockSprite(ImageSprite):
     def __init__(self, what, x=0, y=0):
         self.what = what
+        self.falling = False
         image = block_images[what]
         super(BlockSprite, self).__init__(image, x, y)
 
@@ -40,62 +41,79 @@ class BlockSprite(ImageSprite):
         return (self.what == what)
 
 
-landscape = []
-for y in range(SCREEN_SIZE):
-    landscape.append([])
-    for x in range(SCREEN_SIZE):
-        if x in (0, (SCREEN_SIZE-1)) or y in (0, (SCREEN_SIZE-1)):
-            what = 'wall'
-        elif random.random() < 0.02:
-            what = 'wall' 
-        elif random.random() < 0.1:
-            what = 'boulder'
-        else:
-            what = 'mud'
-        block = BlockSprite(what)
-        block.move_to(x*BLOCK_SIZE, y*BLOCK_SIZE)
-        landscape[-1].append(block)
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-fred = ImageSprite(fred_img)
-fred.move_to(2*BLOCK_SIZE,2*BLOCK_SIZE)
+def block_choices():
+    """More gems, boulders and walls the higher the level"""
+    c1 = ['mud'] * 10
+    c2 = ['wall', 'boulder', 'gem'] * world.level
+    c1.extend(c2)
+    return c1
+
+def make_landscape():
+    landscape = []
+    for y in range(SCREEN_SIZE):
+        landscape.append([])
+        for x in range(SCREEN_SIZE):
+            r = random.random()
+            if x in (0, (SCREEN_SIZE-1)) or y in (0, (SCREEN_SIZE-1)):
+                what = 'wall'
+            else:
+                what = random.choice(block_choices())
+            block = BlockSprite(what)
+            block.move_to(x*BLOCK_SIZE, y*BLOCK_SIZE)
+            landscape[-1].append(block)
+    return landscape
 
 def coords(sprite):
     cx, cy = sprite.pos()
     cx = int(cx/BLOCK_SIZE); cy = int(cy/BLOCK_SIZE)
     return (cx, cy)
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+def all_blocks_of(what):
+    b = []
+    for rows in world.landscape:
+        for i in rows:
+            if i and i.is_a(what):
+                b.append(i)
+    return b
 
-world = Struct( level=1, status='play', landscape=landscape, fred=fred )
+def all_boulders():
+    return all_blocks_of('boulder')
+
+def all_gems():
+    return all_blocks_of('gem')
 
 def what_is_next_to(sprite, dx, dy):
     cx, cy = coords(sprite)
     if sprite != fred and (cx+dx, cy+dy) == coords(fred):
         return fred
-    try:
-        return landscape[cy+dy][cx+dx]    
-    except IndexError:
-        return EdgeSprite()
+    return world.landscape[cy+dy][cx+dx]    
 
 def can_move(dx, dy):
     sprite = what_is_next_to(fred, dx, dy)
-    return sprite is None or sprite.is_a('mud')
+    return sprite is None or sprite.is_a('mud') or sprite.is_a('gem')
 
 def set_landscape(a_pair, what):
     x, y = a_pair
-    landscape[y][x] = what
+    world.landscape[y][x] = what
 
-def delete_mud():
+def clear_block():
+    """Clear the block that fred just landed on"""
     x, y = coords(fred)
-    mud = landscape[y][x]
-    if mud:
-        landscape[y][x] = None
-        mud.delete()
+    block = world.landscape[y][x]
+    if block:
+        world.landscape[y][x] = None
+        if block.is_a('gem'): world.gems_left -= 1
+        block.delete()
 
 def move(dx, dy):
     if can_move(dx, dy):
         fred.move(dx*BLOCK_SIZE, dy*BLOCK_SIZE)
-        delete_mud()
+        clear_block()
+        if world.gems_left <= 0:
+            banner("Well done!")
+            world.status = 'next_level'
     elif dy == 0:
         next_block = what_is_next_to(fred, dx, 0)
         next_next_block = what_is_next_to(fred, dx*2, 0)
@@ -107,6 +125,26 @@ def move(dx, dy):
             # Now we can move too
             fred.move(dx*BLOCK_SIZE, 0)
         
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# World set up
+#
+
+world = Struct( level=1, status='play', fred=None, 
+                landscape=None, gems_left=None )
+
+# These require previous world things to be defined
+world.landscape = make_landscape() 
+
+fred = ImageSprite(fred_img)
+fred.move_to(2*BLOCK_SIZE,2*BLOCK_SIZE)
+world.fred = fred
+
+world.gems_left = len(all_gems())
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Events and actions
+#
+
 def move_left(event):
     move(-1, 0)
 
@@ -119,24 +157,28 @@ def move_up(event):
 def move_down(event):
     move(0, 1)
 
-def all_boulders():
-    b = []
-    for rows in landscape:
-        for i in rows:
-            if i and i.is_a('boulder'):
-                b.append(i)
-    return b
-
 def boulders_fall():
     for b in all_boulders():
-        if what_is_next_to(b, 0, 1) is None:
+        what = what_is_next_to(b, 0, 1)
+        if what is None:
             set_landscape(coords(b), None)
             b.move(0, BLOCK_SIZE)
+            b.falling = True
             set_landscape(coords(b), b)
+        else:
+            if what is world.fred and b.falling:
+                banner("Ouch!")
+                world.status = 'end'
+            else:
+                b.falling = False
+
+def check_status():
+    pass
 
 when_key_pressed('<Left>', move_left)
 when_key_pressed('<Right>', move_right)
 when_key_pressed('<Up>', move_up)
 when_key_pressed('<Down>', move_down)
 forever(boulders_fall, 200)
+forever(check_status, 1000)
 mainloop()
