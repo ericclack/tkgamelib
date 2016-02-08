6a1f5e2144eb972d21d0a675272a655e69c6c30b
Eric Clack <eric@bright-interactive.com>
2016-02-08 16:49:17 +0000

    A variety of trees

--- examples/fractal-tree2.py
+++ examples/fractal-tree2.py
@@ -5,12 +5,13 @@ import random, sys
 sys.path.append('..')
 from geekclub.pyscratch import *
 
-BRANCHES = 6
-BRANCH_ANGLE=30
+# How much to change the length on each recursion
 LENGTH_DELTA=0.5
+# How random to make the tree
 RANDOMNESS=0.02
 
 create_canvas()
+
 
 def random_change(v):
     # This random number is small if RANDOMNESS is small
@@ -22,21 +23,25 @@ def random_change(v):
         return v * (1 + r)
 
 
-def tree(x, y, length, angle):
+def tree(branches, branch_angle, x, y, length, angle):
 
     if length < 5: return
-        
+    
+    # Draw this branch
     length = random_change(length)
     angle = random_change(angle)
     x2, y2 = translate_point(x, y, length, angle)
     canvas().create_line(x,y, x2, y2, width=length/20)
     
-    for i in range(BRANCHES):
-        tree(x2, y2, length * LENGTH_DELTA, 
-             angle+(BRANCH_ANGLE*(i-(BRANCHES/2))))
+    # Draw a smaller tree at the end of this branch
+    for i in range(branches):
+        tree(branches, branch_angle, x2, y2, 
+             length * LENGTH_DELTA, 
+             angle+(branch_angle*(i-(branches/2))))
 
 for y in range(2):
     for x in range(2):
-        tree(200 + x*350, 380 + y*350, length=180, angle=270)
+        v = (1+x)+y*2
+        tree(3+v, 50 - (7*v), 200 + x*350, 380 + y*360, length=170, angle=270)
 
 mainloop()
