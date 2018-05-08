bdd938a45cfb0aa0da014811b3922d8029a17e00
Eric Clack <eric@bn7.net>
2018-05-08 08:40:13 +0100

    Progress on #5, added comments

--- examples/sound1.py
+++ examples/sound1.py
@@ -2,10 +2,12 @@ import sys
 sys.path.append('..')
 from geekclub.pyscratch import *
 
+# Install simpleaudio with: pip3 install simpleaudio
 import simpleaudio as sa
 
 create_canvas()
 
+# Not really necessary for this example, but why not:
 sprite = ImageSprite('images/face.gif')
 sprite.centre()
 
@@ -14,6 +16,6 @@ laser = sa.WaveObject.from_wave_file('sounds/laser.wav')
 def laser_sound():
     laser.play()
 
-forever(laser_sound, 1000)
+forever(laser_sound, 500)
 
 mainloop()
