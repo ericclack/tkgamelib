283503478091c57b50e2712fd50a93c541d99f58
Eric Clack <eric@bn7.net>
2018-05-08 10:29:04 +0100

    pip3 not pip

--- geekclub/sound.py
+++ geekclub/sound.py
@@ -7,7 +7,7 @@
 See examples in the folder examples, or more info on github.com:
 https://github.com/ericclack/geekclub/sound1.py
 
-To install: pip install simpleaudio
+To install: pip3 install simpleaudio
 Get free to use sounds here: https://freesound.org/
 
 Author: Eric Clack, eric@bn7.net
@@ -19,7 +19,7 @@ class DummySoundLib():
 
     def __init__(self):
         print("Can't play sound!")
-        print("You need to install simpleaudio with `pip install simpleaudio`")
+        print("You need to install simpleaudio with `pip3 install simpleaudio`")
         DummySoundLib.WaveObject = self
 
     def from_wave_file(self, file):
