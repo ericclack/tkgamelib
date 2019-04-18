e557317f62b21906bcee8515a39c41ae13a987a2
Eric Clack <eric@bn7.net>
2019-04-18 22:33:01 +0100

    First Kata

---
+++ kata/k01_easy.py
@@ -0,0 +1,15 @@
+# Copyright 2019, Eric Clack, eric@bn7.net
+# This program is distributed under the terms of the GNU General Public License
+
+"""Fix the errors, or write the code to make the tests pass.
+
+An easy one to get you started"""
+
+a = 57
+b = 21
+sum = a - b
+
+# These are the tests, fix the code above to make them work
+
+assert sum == 78, "Oops, the sum is incorrect"
+print("All tests passed, well done!")
