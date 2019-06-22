1b60386a910cec236396b5247d2fe023b4a6270b
Eric Clack <eric@bn7.net>
2019-06-22 11:43:16 +0100

    Collision dection

--- bigbang/orbits.py
+++ bigbang/orbits.py
@@ -8,17 +8,23 @@ from orbits_lib import *
 
 """
 
-create_canvas("Orbits", 1000, 800, background="black")
+create_canvas("Orbits", 1000, 900, background="black")
 
 # Our own gravity constant
 G = 0.0005
+
+# Does the game end if we hit a planet?
+COLLISIONS = True
 
 # ---------------------------------------------------------
 # Create your sprite objects
  
 clicks=[]
+mx, my = canvas_width()/2,canvas_height()/2
+
 planets=[
-    Sprite(canvas().create_oval(300,250, 650,600, fill="white")),
+    Sprite(canvas().create_oval(mx-150, my-150, mx+150, my+150,
+                                fill="white")),
 ]
 ship = Sprite(canvas().create_oval(0,0, 25,25, fill="red"))
 ship.max_speed = 100 # Fast!
@@ -30,13 +36,17 @@ ship.max_speed = 100 # Fast!
 
 def gravity():
     for p in planets:
-        d = max(p.width/5, ship.distance_between(p))
+        d = ship.distance_between(p)
+        if COLLISIONS and d < p.width/2:
+            end_game("You hit a planet!", fn=restart, ms=2000, fill="red")
         f = G*(4*(p.width/2)**3/d**2)
         ship.accelerate_towards(p.centre_x, p.centre_y, f)
 
 def move_ship():
     gravity()
     ship.move_with_speed()
+    if COLLISIONS and not (0 < ship.x < canvas_width()) or not (0 < ship.y < canvas_height()):
+            end_game("You left the solar system!", fn=restart, ms=2000, fill="red")
 
 def propel_ship(event):
     x, y = event.x, event.y
@@ -57,6 +67,7 @@ def restart():
     ship.move_to(80, CANVAS_HEIGHT-100)
     ship.speed_x = ship.speed_y = 0
     delete_all(clicks)
+    restart_game()
 
 # ---------------------------------------------------------
 # How will the user control the game? What will other
